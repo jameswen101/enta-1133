862d73197a3d413779b1828445632379cd559a27
jameswen101 <148604180+jameswen101@users.noreply.github.com>
2024-09-06 14:13:44 -0700

    Lab Assignment 1

--- GD12_1133_Lab1_wen_james/Program.cs
+++ GD12_1133_Lab1_wen_james/Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("James Wen ENTA 1133");
+            Console.WriteLine("Hello, World! James Wen ENTA 1133, Sept 6, 2024");
+            Console.WriteLine("Decimal, Binary, and Hexadecimal Numbers:");
+            Console.WriteLine("0, 00000000, 00");
+            Console.WriteLine("1, 00000001, 01");
+            Console.WriteLine("2, 00000010, 02");
+            Console.WriteLine("4, 00000100, 04");
+            Console.WriteLine("8, 00001000, 08");
+            Console.WriteLine("16, 00010000, 10");
+            Console.WriteLine("32, 00100000, 20");
+            Console.WriteLine("64, 01000000, 40");
+            Console.WriteLine("100, 01100100, 64");
+            Console.WriteLine("255, 11111111, FF");
+            Console.WriteLine("Goodbye! James Wen ENTA 1133, Sept 6, 2024");
         }
     }
 }
+
