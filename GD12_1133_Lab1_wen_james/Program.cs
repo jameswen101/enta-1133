d206c50e096fedbeb0a512038a42ed806ada361b
jameswen101 <148604180+jameswen101@users.noreply.github.com>
2024-09-06 12:15:01 -0700

    Explore Visual Studio
    Basic Project

---
+++ GD12_1133_Lab1_wen_james/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_wen_james
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("James Wen ENTA 1133");
+        }
+    }
+}
