a6ee5e8c99e0d2ec6dbb59ce90a1ca3e2c6dfc62
jameswen101 <148604180+jameswen101@users.noreply.github.com>
2024-09-13 16:02:23 -0700

    Files Split Apart

---
+++ GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class GameManager
+{
+    private bool againstComputer = true;
+
+    public void PlayGame()
+    {
+        Console.WriteLine("Welcome to Die vs Die!");
+        DieRoller dieRollerInstance;
+        dieRollerInstance = new DieRoller(); //Create an instance of DieRoller
+        Console.Write("This is a game about rolling dice with ");
+        Console.Write(dieRollerInstance.numberOfSides);
+        Console.Write(" sides! ");
+        dieRollerInstance.Description();
+        dieRollerInstance.Roll();
+    }
+
+
+}
