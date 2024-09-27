f05ef8a907541e381da3069b6be9be598dcc4f6a
jameswen101 <148604180+jameswen101@users.noreply.github.com>
2024-09-27 10:04:01 -0700

    Lab 3 Upload

---
+++ DieRoller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class DieRoller
+{
+    public int numberOfSides; 
+    public int roll; //value of number rolled
+    Random random = new Random(); //Create an instance of Random
+
+    public int Roll()
+    {
+        roll = random.Next(1, numberOfSides + 1); //generates a random number within the die range
+        return roll;
+    }
+    public DieRoller(int numberOfSides) //constructor, number of sides a parameter for each object
+    {
+        this.numberOfSides = numberOfSides;
+    }
+}
+
---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Player
+{
+	public int score = 0;
+    public int turnsTaken = 0;
+	public bool sixRolled, eightRolled, twelveRolled, twentyRolled = false; //stores whether each die has been rolled or not
+
+	public Player() //constructor
+	{
+       
+	}
+}
